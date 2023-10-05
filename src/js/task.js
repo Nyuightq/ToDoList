4b51c6248e70385c440760356d6e9a6393fbaab1
Nyuightq <nyuightq@gmail.com>
2023-10-05 22:23:51 +0800

    Local Storage function
    - Improve drag n drop while creating new element has difficult to drop.
    - Create local storage function, but without edit support

--- src/js/task.js
+++ src/js/task.js
@@ -2,7 +2,7 @@ window.onload = function(){
     initTooltip();
 }
 function initTooltip(){
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     tooltipTriggerList.map(function (tooltipTriggerEl) {
         const isInit = tooltipTriggerEl.hasAttribute('data-tooltip-initialized');
         if(!isInit){
@@ -24,23 +24,91 @@ function deleteTooltip(){
 
 
 // Task functions
+    if(localStorage.getItem("TaskList") === null){
+        var todos = [];
+    }
+    else{
+        var todos = JSON.parse(localStorage.getItem("TaskList"));
+        console.log(todos);
 
-    var todos = [];
-    var isEditing = false;
+        // Initialize local storage data.
+        todos.forEach(function (task){
+            const script = `
+                <div draggable="true" class="task border border-dark-subtle p-2 rounded-3 mb-3" id="${task.id}">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <span class="editable pe-2 text-break" style="user-select: none;">
+                        ${task.content}
+                        </span>
+                    <div class="d-flex justify-content-end">
+                        <button type="button" class="btn-edit btn btn-light p-1 d-flex rounded-circle" data-bs-toggle="tooltip" data-bs-title="Edit task"
+                                onclick="editTask(this)">
+                            <i class="fa-solid fa-pencil"></i>
+                        </button>
+                        <button type="button" class="btn-delete btn btn-light p-1 d-flex rounded-circle" data-bs-toggle="tooltip" data-bs-title="Delete task"
+                        onclick="deleteTask(this)">
+                            <i class="fa-solid fa-circle-xmark"></i>
+                        </button>
+                    </div>
+                    </div>
+                    <hr class="my-1">
+                    <div class="d-flex justify-content-end flex-wrap">
+                        <div class="badge text-bg-primary py-0 mx-1">
+                            <div class="d-flex align-items-center justify-content-end text-end" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Progressed time">
+                                <span class="editable align-items-center center p-2 px-1" value=${task.processedTime}>
+                                    -
+                                </span>
+                                <i class="fa-solid fa-hourglass-half"></i>
+                            </div>
+                        </div>
+                        <div class="badge text-bg-warning py-0 mx-1">
+                            <div class="d-flex align-items-center justify-content-end text-end" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Estimate time">
+                                <span class="editable align-items-center center p-2 px-1" value=${task.estTime}>
+                                    -
+                                </span>
+                                <i class="fa-solid fa-stopwatch"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(script, 'text/html');
+            let taskCode = doc.body.firstChild;
 
-    var input = document.getElementById("newTaskName");
+            let taskContainer;
+            switch(task.status){
+                case "workingList": taskContainer = document.getElementById("workingList");
+                                break;
+                case "doneList":    taskContainer = document.getElementById("doneList");
+                                break;
+                default:        taskContainer = document.getElementById("pendingList");
+            }
+            taskContainer.appendChild(taskCode);
+        });
+    }
+    const input = document.getElementById("newTaskName");
     input.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         event.preventDefault();
         document.getElementById("btn-create").click();
-    }
-    });
+    }});
     
+
+
     function newTask(){
-        var inputValue = document.getElementById("newTaskName").value;
+        const inputValue = document.getElementById("newTaskName").value;
+        const task = {
+            id: Date.now(),
+            content: inputValue,
+            status: "pendingList",
+            estTime: 0,
+            processedTime: 0
+        };
+        todos.push(task);
+        localStorage.setItem("TaskList", JSON.stringify(todos));
+        
         if (inputValue) {
-            var taskCode = document.createElement("div");
-            taskCode.innerHTML = `
+            const script = `
                 <div draggable="true" class="task border border-dark-subtle p-2 rounded-3 mb-3" id="${Date.now()}">
                     <div class="d-flex align-items-center justify-content-between">
                         <span class="editable pe-2 text-break" style="user-select: none;">
@@ -61,7 +129,7 @@ function deleteTooltip(){
                     <div class="d-flex justify-content-end flex-wrap">
                         <div class="badge text-bg-primary py-0 mx-1">
                             <div class="d-flex align-items-center justify-content-end text-end" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Progressed time">
-                                <span class="editable align-items-center center p-2 px-1">
+                                <span class="editable align-items-center center p-2 px-1" value=0>
                                     -
                                 </span>
                                 <i class="fa-solid fa-hourglass-half"></i>
@@ -69,7 +137,7 @@ function deleteTooltip(){
                         </div>
                         <div class="badge text-bg-warning py-0 mx-1">
                             <div class="d-flex align-items-center justify-content-end text-end" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Estimate time">
-                                <span class="editable align-items-center center p-2 px-1">
+                                <span class="editable align-items-center center p-2 px-1" value=0>
                                     -
                                 </span>
                                 <i class="fa-solid fa-stopwatch"></i>
@@ -78,22 +146,31 @@ function deleteTooltip(){
                     </div>
                 </div>
             `;
-        var taskContainer = document.getElementById("pendingList");
-        taskContainer.appendChild(taskCode);
-        document.getElementById("newTaskName").value = "";
-        initTooltip();
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(script, 'text/html');
+            let taskCode = doc.body.firstChild;
+
+            const taskContainer = document.getElementById("pendingList");
+            taskContainer.appendChild(taskCode);
+            document.getElementById("newTaskName").value = "";
+            initTooltip();
         }
     }
 
     function deleteTask(e) {
         deleteTooltip();
+        // Delete by filtering task by using div's ID.
+        todos = todos.filter(function(todo){
+            return todo.id != e.parentElement.parentElement.parentElement.id;
+        })
+        // Update localStorage
+        localStorage.setItem("TaskList", JSON.stringify(todos)); 
         e.parentElement.parentElement.parentElement.remove();
-        initTooltip();
     }
 
     function editTask(editButton) {
-        var task = editButton.parentElement.parentElement.parentElement;
-        var editableElements = task.querySelectorAll(".editable");
+        const task = editButton.parentElement.parentElement.parentElement;
+        const editableElements = task.querySelectorAll(".editable");
 
         if (!editButton.matches(".editingBtn")) {
             editableElements.forEach(function (element) {
@@ -116,6 +193,38 @@ function deleteTooltip(){
         }
     }
 
+    // function updateTaskInfo(task){
+    //     todos = todos.map(function(todo){
+    //         console.log("pass");
+    //         if(todo.id == task.id){
+    //             if(todo.content != task.content)
+    //                 todo.content = task.content;
+                
+    //             if(todo.estTime != task.estTime)
+    //                 todo.estTime = task.estTime;
+
+    //             if(todo.processedTime != task.processedTime)
+    //                 todo.processedTime = task.processedTime;
+
+    //             return todo;
+    //         }
+    //         return todo;
+    //     });
+    // }
+
+    function updateTaskStatus(task){
+        todos = todos.map(function(todo){
+            if(todo.id == task.id){
+                const type = task.parentElement.id;
+                todo.status = type;
+                return todo;
+            }
+            return todo;
+        });
+        // Update localStorage
+        localStorage.setItem("TaskList", JSON.stringify(todos)); 
+    }
+
 
 //
 // Task drag and drop function, with instant arrangement.
@@ -131,8 +240,6 @@ function deleteTooltip(){
                 return
 
             sourceNode = e.target;
-            console.log(e.target);
-
             setTimeout(() => {
                 e.target.classList.add('moving');
             }, 0);
@@ -141,6 +248,7 @@ function deleteTooltip(){
 
         list.ondragend = e => {
             e.target.classList.remove('moving');
+            updateTaskStatus(e.target);
         }
 
         list.ondragover = e => {
@@ -162,34 +270,5 @@ function deleteTooltip(){
             } else {
                 list.insertBefore(sourceNode, e.target);
             }
-
-            last([e.target, sourceNode]);
-            console.log(children.indexOf(sourceNode) + "+" + children.indexOf(e.target));
         };
     });
-
-    function record(eleAll){
-        for (let i=0; i < eleAll.length; i++){
-            const {top, left} = eleAll[i].getBoundingClientRect();
-            eleAll[i]._top = top;
-            eleAll[i]._left = left;
-        }
-    }
-
-    function last(eleAll){
-        for(let i=0;i<eleAll.length;i++){
-            const dom=eleAll[i];
-            const {top,left}=dom.getBoundingClientRect();
-            if(dom._left){
-                dom.style.transform=`translate3d(${dom._left-left}px,${dom._top-top}px,0px)`;
-                let rafId=requestAnimationFrame(function(){
-                    dom.style.transition='transform 0.3s ease-out';
-                    dom.style.transform='none';
-                })
-                dom.addEventListener('transitionend',()=>{
-                    dom.style.transition='none';
-                    cancelAnimationFrame(rafId);
-                })
-            }
-        }
-    }
